b7fe121cfc3bc994914d1dd2eafc746dc773b85b
thanhvp <phuocthanh20cgh@gmail.com>
2023-05-23 21:06:22 +0700

    upload server

--- ecomern-yt-backend/server.js
+++ ecomern-yt-backend/server.js
@@ -2,10 +2,10 @@ import express from 'express';
 import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import seedRouter from './routes/seedRoutes.js';
-import productRouter from './routes/productRoutes.js';
-import userRouter from './routes/userRoutes.js';
-import orderRouter from './routes/orderRoutes.js';
+import seedRouter from '../routes/seedRoutes.js';
+import productRouter from '../routes/productRoutes.js';
+import userRouter from '../routes/userRoutes.js';
+import orderRouter from '../routes/orderRoutes.js';
 
 dotenv.config();
 
