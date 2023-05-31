5c77b2f095616e6e0fa7f3c0c762c5ad7560bc6f
thanhvp <phuocthanh20cgh@gmail.com>
2023-05-31 16:12:16 +0700

    delete fe

--- ecomern-yt-frontend/src/components/MessageBox.js
+++
@@ -1,9 +0,0 @@
-
-import Alert from 'react-bootstrap/Alert'
-export default function MessageBox(props){
-  
-     return (<Alert variant={props.variant ||'nifo'}>
-     {props.children}
-
- </Alert>)
-}
